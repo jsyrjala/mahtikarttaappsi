88dc30e3fe0a1d6a41ab17f93773df44d28b7ad9
Juha Syrjälä <juha.syrjala@iki.fi>
2015-11-07 00:59:45 +0200

    Randomize order of cities in fixture server

--- src/test/server/fixture-server.js
+++ src/test/server/fixture-server.js
@@ -13,13 +13,15 @@ app.listen(3100)
 
 function sender(ws) {
   console.log('sender')
-  var cityIndex = 0
+  function randomCityIndex() {
+    return Math.floor(Math.random() * geodata.features.length)
+  }
+  var cityIndex = randomCityIndex()
   var coordinateIndex = 0
-
 
   return function sendCoordinate() {
     function nextCity() {
-      cityIndex++
+      cityIndex = randomCityIndex()
       coordinateIndex = 0
     }
     function nextCoordinate() {
@@ -28,10 +30,17 @@ function sender(ws) {
     try {
       var features = geodata.features
       var city = features[cityIndex]
-
+      if(!city.geometry.coordinates) {
+        // e.g has several geometry subgroups (Kotka, Kalajoki, Ii, ...)
+        console.log('Bad city', city)
+        nextCity()
+        nextCoordinate()
+        return
+      }
       var coordinates = city.geometry.coordinates[0]
       if(!coordinates) {
         nextCoordinate()
+        return;
       }
       var coordinate = coordinates[coordinateIndex]
       var cityName = city.properties.name
